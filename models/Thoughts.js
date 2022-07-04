de67bba4457cec3e700c114f0c9f318de370bfb0
Brianna <briannaodell1437@gmail.com>
2022-07-04 14:04:13 -0700

    resave

---
+++ models/Thoughts.js
@@ -0,0 +1,29 @@
+const { Schema, Types } = require("mongoose");
+
+const thoughtsSchema = new Schema(
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    thoughtBody: {
+      type: String,
+      required: true,
+      maxlength: 500,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thoughts = model("thoughts", thoughtsSchema);
+
+module.exports = Thoughts;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
-const User = require('./User');
-const Tag = require('./Tag');
-const Application = require('./Application');
+const User = require("./User");
+const Tag = require("./Tag");
+const Application = require("./Application");
+const Thoughts = require("./Thoughts");
 
 module.exports = { User, Application, Tag };
